27f4b4d448f7f5ae63ba83acb9c10134c8e2ba70
KrasiMut <mutiAITech@proton.me>
2023-07-05 16:26:40 +0300

    Commit 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Welcome to Bulls and Cows Game!");
+        System.out.println("Choose a game mode:");
+        System.out.println("1. Single Player");
+        System.out.println("2. Two Players");
+    }
+}
